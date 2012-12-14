4db4f7d5e187aec1a7dcc95c46c8a87011148d2e
Diwant Vaidya <diwant+aegir.boa.vagrant@gmail.com>
2012-12-14 12:19:57 +0000

    Fixed the regen compass file

--- regen-compass.sh
+++ regen-compass.sh
@@ -10,7 +10,6 @@ echo "== I'm going to regenerate $3 on $1 in $2 ok? =="
 
 echo "Calling compass on $3..."
 THEROOT=`drush @$1 dd`
-echo `ls $THEROOT/profiles/$2/themes`
 compass compile $THEROOT/profiles/$2/themes/$3
 
 echo "...Done!"
