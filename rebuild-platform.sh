37c939bffbdfee9b1cfb7690b73a510df06b16b3
Diwant Vaidya <diwant+o1.vagrant@gmail.com>
2013-01-03 04:56:55 +0000

    Now platform directory variable is independend of hoem directory

--- rebuild-platform.sh
+++ rebuild-platform.sh
@@ -13,21 +13,21 @@ mkdir -p $HOME/temp
 source "$HOME/.diw-scripts/local-vars.sh"
 
 echo "Storing sites folder in the sites bank..."
-mv -f $diwant_platform_directory/$1/sites $HOME/temp/sites.$1
+mv -f "$HOME/$diwant_platform_directory/$1/sites" "$HOME/temp/sites.$1"
 
 echo "Removing old platform..."
-rm -Rf $diwant_platform_directory/$1 
+rm -Rf "$HOME/$diwant_platform_directory/$1"
 
 echo "Make the new platform with drush make..."
-drush make --working-copy  $HOME/build/build-$1.make $diwant_platform_directory/$1
+drush make --working-copy  "$HOME/build/build-$1.make" "$HOME/$diwant_platform_directory/$1"
 
 echo "Remove the new sites directory..."
-rm -Rf $diwant_platform_directory/$1/sites
+rm -Rf "$HOME/$diwant_platform_directory/$1/sites"
 
 echo "Bring back the sites directory..."
-mv -f $HOME/temp/sites.$1 $diwant_platform_directory/$1/sites
+mv -f "$HOME/temp/sites.$1" "$HOME/$diwant_platform_directory/$1/sites"
 
 echo "You should reinstall these sites..."
-echo `ls $diwant_platform_directory/$1/sites/`
+echo `ls $HOME/$diwant_platform_directory/$1/sites/`
 
 echo "...Done!"
